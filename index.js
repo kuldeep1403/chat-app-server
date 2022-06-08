7f8d33d58e06b652014d956e619269ca2568819e
kuldeep1403 <kuldeepsingh.14rewa@gmail.com>
2022-06-08 17:14:40 +0530

    some changes

--- index.js
+++ index.js
@@ -12,14 +12,7 @@ const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-app.use(
-  cors({
-    origin: ["https://justchatting.vercel.app/"],
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 app.use(express.json());
 app.use(cookieParser());
