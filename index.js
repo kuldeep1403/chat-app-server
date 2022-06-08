9457eb23f3acea6655f0888c7783419d5003eb6a
kuldeep1403 <kuldeepsingh.14rewa@gmail.com>
2022-06-08 19:50:17 +0530

    fixing errors

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ mongoose.connect(DB, {
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://justchatting.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
